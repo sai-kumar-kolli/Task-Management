d6533720ac1437587e008769c69c609f370d4c12
Sai Kumar K <>
2024-07-26 15:24:02 +0530

    cleanup

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tasksReducer from '../features/tasks/taskSlice';
-import authReducer from '../features/auth/authSlice';
 
 const store = configureStore({
   reducer: {
     tasks: tasksReducer,
-    auth: authReducer,
   },
 });
 
